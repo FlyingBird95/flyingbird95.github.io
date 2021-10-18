5eb231d316980e46abe9825ab31928ffe8e4bc51
Giraficode <giraficode@gmail.com>
2021-10-18 09:32:43 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
 });
+
+// @ponicode
+describe("getData", () => {
+    let object
+    let inst
+
+    beforeEach(() => {
+        object = [["George", "Pierre Edouard", "Pierre Edouard"], ["Jean-Philippe", "Jean-Philippe", "George"], ["George", "Edmond", "Anas"]]
+        inst = new App.default(object)
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.getData()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("componentDidMount", () => {
+    let object
+    let inst
+
+    beforeEach(() => {
+        object = [["Anas", "Pierre Edouard", "Anas"], ["George", "Michael", "Edmond"], ["Michael", "Anas", "Jean-Philippe"]]
+        inst = new App.default(object)
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
